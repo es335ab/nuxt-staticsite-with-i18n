75ada35c1b22ae9bd200c767677c001d49ece557
es335ab <es335ab@gmail.com>
2018-04-23 20:19:28 +0900

    init

---
+++ nuxt.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  loading: { color: 'cyan' },
+  router: {
+    middleware: 'i18n'
+  },
+  plugins: ['~/plugins/i18n.js'],
+  generate: {
+    routes: ['/', '/about', '/fr', '/fr/about']
+  }
+}
