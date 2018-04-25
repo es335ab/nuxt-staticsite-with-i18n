50d08ffb48f9d8fd12f63e6d9aba890d47e20724
es335ab <es335ab@gmail.com>
2018-04-25 13:49:58 +0900

    partialize header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,36 +1,19 @@
-<template>
-  <div>
-    <header class="Header">
-      <div class="container">
-        <h1 class="Header__Title">Nuxt i18n</h1>
-        <nav class="Header__Menu">
-          <nuxt-link class="Header__Link" :to="$i18n.path('')" exact>
-            {{ $t('links.home') }}
-          </nuxt-link>
-          <nuxt-link class="Header__Link" :to="$i18n.path('about')" exact>
-            {{ $t('links.about') }}
-          </nuxt-link>
-          <nuxt-link class="Header__Link" v-if="$i18n.locale !== 'fr'" :to="`/fr`" active-class="none" exact>
-            {{ $t('links.french') }}
-          </nuxt-link>
-          <nuxt-link class="Header__Link" v-if="$i18n.locale !== 'ja'" :to="`/ja`" active-class="none" exact>
-            {{ $t('links.japanese') }}
-          </nuxt-link>
-          <nuxt-link class="Header__Link" v-else :to="$route.fullPath.replace(/^\/[^\/]+/, '')" active-class="none" exact>
-            {{ $t('links.english') }}
-          </nuxt-link>
-        </nav>
-      </div>
-    </header>
-    <nuxt/>
-  </div>
+<template lang="pug">
+  div
+    global-header
 </template>
 
 <script>
-export default {}
+import GlobalHeader from '~/layouts/parts/GlobalHeader.vue'
+
+export default {
+  components: {
+    GlobalHeader,
+  },
+}
 </script>
 
-<style>
+<style lang="scss">
 *, *:before, *:after {
   padding: 0;
   margin: 0;
@@ -44,53 +27,5 @@ html, body {
   font-family: "Source Sans Pro", Arial, sans-serif;
   height: 100vh;
   margin: 0;
-}
-.container {
-  width: 95%;
-  margin: 0 auto;
-}
-.container:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-.Header {
-  color: #fff;
-  height: 80px;
-  line-height: 80px;
-  background-color: #2e2f30;
-}
-.Header__Title {
-  float: left;
-  font-weight: 300;
-  font-size: 30px;
-}
-.Header__Menu {
-  float: right;
-}
-.Header__Link {
-  font-size: 16px;
-  color: #fff;
-  border: 1px solid #fff;
-  padding: 7px 12px;
-  text-transform: uppercase;
-  text-decoration: none;
-  border-radius: 5px;
-  margin-left: 10px;
-}
-.Header__Link:hover {
-  color: #2e2f30;
-  background-color: #fff;
-}
-.nuxt-link-active {
-  color: cyan;
-}
-.Content {
-  padding: 50px 0;
-  text-align: center;
-}
-.Content__Title {
-  font-weight: 300;
-  padding-bottom: 30px;
 }
 </style>
---
+++ layouts/parts/GlobalHeader.vue
@@ -0,0 +1,58 @@
+<template lang="pug">
+  div
+    header.Header
+    div.container
+      h1.Header__Title Nuxt i18n
+      nav.Header__Menu
+        nuxt-link.Header__Link(:to="$i18n.path('')" exact) {{ $t('links.home') }}
+        nuxt-link.Header__Link(:to="$i18n.path('about')" exact) {{ $t('links.about') }}
+        nuxt-link.Header__Link(:to="`/fr`" exact) {{ $t('links.french') }}
+        nuxt-link.Header__Link(:to="`/ja`" exact) {{ $t('links.japanese') }}
+        nuxt-link.Header__Link(:to="`/`" exact) {{ $t('links.english') }}
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.Header {
+  color: #fff;
+  height: 80px;
+  line-height: 80px;
+  background-color: #2e2f30;
+}
+.Header__Title {
+  float: left;
+  font-weight: 300;
+  font-size: 30px;
+}
+.Header__Menu {
+  float: right;
+}
+.Header__Link {
+  font-size: 16px;
+  color: #fff;
+  border: 1px solid #fff;
+  padding: 7px 12px;
+  text-transform: uppercase;
+  text-decoration: none;
+  border-radius: 5px;
+  margin-left: 10px;
+}
+.Header__Link:hover {
+  color: #2e2f30;
+  background-color: #fff;
+}
+.nuxt-link-active {
+  color: cyan;
+}
+.Content {
+  padding: 50px 0;
+  text-align: center;
+}
+.Content__Title {
+  font-weight: 300;
+  padding-bottom: 30px;
+}
+</style>
