00ccd5efad1d0799a4abe3e5e9a22eb6d4fe9f54
es335ab <es335ab@gmail.com>
2018-05-01 16:26:28 +0900

    add users

---
+++ pages/_lang/users/_id.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  div
+    global-header
+    div.container
+      h1 {{ $t('user.title') }}
+      div.user-id {{ id }}
+      div.user-name {{ name }}
+      div.user-age {{ ageWithUnit }}
+</template>
+
+<script>
+import usersData from '~/data/users.json'
+import GlobalHeader from '~/layouts/parts/GlobalHeader.vue'
+
+export default {
+  head() {
+    return { title: this.$t('user.title') }
+  },
+
+  async asyncData({ params, error }) {
+    const user = usersData.filter((item) => { return item.id === Number(params.id) })[0]
+    return user
+  },
+
+  components: {
+    GlobalHeader,
+  },
+
+  computed: {
+    ageWithUnit() {
+      return `${this.age}歳`
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ pages/users/_id.vue
@@ -0,0 +1,4 @@
+<script>
+import User from '~/pages/_lang/users/_id.vue'
+export default User
+</script>
